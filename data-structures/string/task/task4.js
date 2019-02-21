d684aa6773830051ebc75f34b5caaaad671120da
balgennady <balgena2@gmail.com>
2019-02-21 16:16:51 +0200

    string.taslk4 решена

--- data-structures/string/task/task4.js
+++ data-structures/string/task/task4.js
@@ -4,3 +4,13 @@
 // Создайте функцию extractCurrencyValue(str),
 // которая будет из такой строки выделять число-значение, в данном случае 120.
 //#endregion
+
+/**
+ * выделяет число-значение из строки
+ * @param {string} str исходная строка
+ */
+function extractCurrencyValue(str) {
+    var val = 0;
+    val = +str.slice(1);
+    return val;
+}
