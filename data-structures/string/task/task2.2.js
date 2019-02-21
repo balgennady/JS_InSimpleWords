2253669ee563ff14a5ff151a6132dcec1682b5a2
balgennady <balgena2@gmail.com>
2019-02-21 15:40:36 +0200

    string.task2 решена

---
+++ data-structures/string/task/task2.2.js
@@ -0,0 +1,21 @@
+//#region Проверьте спам
+// Напишите функцию checkSpam(str), которая возвращает true,
+// если строка str содержит „viagra“ или „XXX“, а иначе false.
+// Функция должна быть нечувствительна к регистру:
+//#endregion
+
+/**
+ * возвращает результат проверки строки на содерзание слов,
+ * функция нечувствительна к регистру
+ * @param {string} str строка для проверки
+ */
+function checkSpam(str) {
+
+    const lowerStr = str.toLowerCase();
+    return !!( ~lowerStr.indexOf('viagra') || ~lowerStr.indexOf('xxx') );
+    
+}
+
+console.log(checkSpam('buy ViAgRA now') );  // true
+console.log(checkSpam('free xxxxx'));       // true
+console.log(checkSpam("innocent rabbit"));  // false
