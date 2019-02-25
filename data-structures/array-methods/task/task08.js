b9540513a48dc31c7d51d0fa7f2b35ea4e5356ad
balgennady <balgena2@gmail.com>
2019-02-25 22:37:38 +0200

    папка переименована

---
+++ data-structures/array-methods/task/task08.js
@@ -0,0 +1,15 @@
+//#region Сортировка объектов
+// Напишите код, который отсортирует массив объектов people по полю age.
+// Выведите список имён в массиве после сортировки.
+//#endregion
+
+var vasya = { name: "Вася", age: 23 };
+var masha = { name: "Маша", age: 18 };
+var vovochka = { name: "Вовочка", age: 6 };
+
+var people = [ vasya , masha , vovochka ];
+
+// ... ваш код ...
+
+// теперь people: [vovochka, masha, vasya]
+console.log(people[0].age) // 6
