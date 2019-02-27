8339eaa35adf531080f375f214c149c4b5184734
balgennady <balgena2@gmail.com>
2019-02-27 18:23:55 +0200

    задача решена

--- data-structures/array-methods/task/task04.js
+++ data-structures/array-methods/task/task04.js
@@ -4,11 +4,20 @@
 // все числа вне диапазона a..b. То есть,
 // проверка имеет вид a ≤ arr[i] ≤ b.
 // Функция должна менять сам массив и ничего не возвращать.
-// Например:
 //#endregion
 
 arr = [5, 3, 8, 1];
 
+function filterRangeInPlace(arr, a, b) {
+    for (var i = 0; i < arr.length; i++) {
+        if (!(a <= arr[i] && arr[i] <= b)) {
+            arr.splice(i, 1);
+            i--;
+        }
+
+    }
+}
+
 filterRangeInPlace(arr, 1, 4); // удалены числа вне диапазона 1..4
 
-console.log( arr ); // массив изменился: остались [3, 1]
+console.log(arr); // массив изменился: остались [3, 1]
