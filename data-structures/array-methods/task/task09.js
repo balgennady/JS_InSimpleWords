4dc4b9c1d3279a806517231a48a8e069abb37768
balgennady <balgena2@gmail.com>
2019-03-01 19:45:07 +0200

    задача решена

--- data-structures/array-methods/task/task09.js
+++ data-structures/array-methods/task/task09.js
@@ -30,3 +30,21 @@ var list = { value: 1 };
 list.next = { value: 2 };
 list.next.next = { value: 3 };
 list.next.next.next = { value: 4 };
+
+function printListLoop(list) {
+    while (list != null) {
+        console.log(list.value);
+        list = list.next;
+    }
+}
+
+function printListRec (list) {
+    if (list.next) {
+        printListRec(list.next);
+	}
+	console.log(list.value);
+}
+
+printListLoop(list);
+console.log('=================================');
+printListRec(list);
