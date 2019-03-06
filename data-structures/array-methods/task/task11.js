7fb103bb131bb4500725ee2790dafcacf5f470bd
balgennady <balgena2@gmail.com>
2019-03-06 08:23:08 +0200

    задача решена

--- data-structures/array-methods/task/task11.js
+++ data-structures/array-methods/task/task11.js
@@ -5,11 +5,32 @@
 //#endregion
 
 function unique(arr) {
-  /* ваш код */
+
+	var delarr = [];
+
+	for (let firstStr = 0; firstStr < arr.length - 1; firstStr++) {
+
+		// console.log("firstStr: " + arr[firstStr]);
+		var str1 = arr[firstStr];
+
+		for (var secondStr = firstStr + 1; secondStr < arr.length; secondStr++) {
+
+			// console.log("secondStr: " + arr[secondStr]);
+			var str2 = arr[secondStr];
+
+			if (str1 == str2) {
+				// console.log( arr[firstStr] + " = " + arr[secondStr])
+				delarr.push(arr.splice(secondStr, 1));
+				secondStr--;
+			}
+		}
+
+	}
+	return arr;
 }
 
 var strings = ["кришна", "кришна", "харе", "харе",
-  "харе", "харе", "кришна", "кришна", "8-()"
+	"харе", "харе", "кришна", "кришна", "8-()"
 ];
 
-console.log( unique(strings) ); // кришна, харе, 8-()
+console.log(unique(strings)); // кришна, харе, 8-()
