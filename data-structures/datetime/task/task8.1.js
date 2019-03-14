23c87e03e5edd61efe2ab9d89500a4a054bb1a8a
balgennady <balgena2@gmail.com>
2019-03-14 13:48:09 +0200

    исправления

--- data-structures/datetime/task/task8.1.js
+++ data-structures/datetime/task/task8.1.js
@@ -9,24 +9,24 @@
  * @param {Date} date исходная дата
  */
 function formatDate(date) {
-    var res = '';
+	var res = '';
 
-    const inDate = new Date(date);
+	const inDate = new Date(date);
 
-    var year = inDate.getFullYear();
-    year = year.toString().slice(2,4);
+	var year = inDate.getFullYear();
+	year = year.toString().slice(2,4);
 
-    var month = (inDate.getMonth() + 1).toString();
-    if (month.length < 2)
-        month = '0' + month;
+	var month = (inDate.getMonth() + 1).toString();
+	if (month.length < 2)
+		month = '0' + month;
 
-    var day = inDate.getDate().toString();
-    if (day.length < 2)
-        day = '0' + day;
+	var day = inDate.getDate().toString();
+	if (day.length < 2)
+		day = '0' + day;
 
-    res = day + '.' + month + '.' + year;
+	res = day + '.' + month + '.' + year;
 
-    return res;
+	return res;
 }
 
 console.log(formatDate(new Date(2000, 0, 1)));
